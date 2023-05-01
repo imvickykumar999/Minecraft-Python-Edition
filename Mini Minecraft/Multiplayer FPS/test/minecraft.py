1ae762eacb6bc103a6d55cc4c01b1ca4db046ccb
imvickykumar999 <imvickykumar999@gmail.com>
2023-05-01 16:00:13 +0530

    multiplayer.png

--- Mini Minecraft/Multiplayer FPS/test/minecraft.py
+++ Mini Minecraft/Multiplayer FPS/test/minecraft.py
@@ -1,17 +1,16 @@
 
+import socket, ast
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 
-import socket
 HOST = '127.0.0.1'
 PORT = '8080'
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((HOST, int(PORT)))
 app = Ursina()
+# client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 class Voxel(Button):
-    def __init__(self, position=(0,0,0)):
+    def __init__(self, position=(0.0,0.0,0.0)):
 
         super().__init__(
             parent=scene,
@@ -27,20 +26,32 @@ for z in range(8):
     for x in range(8):
         voxel = Voxel(position=(x,0,z))
 
+def update():
+    if player.y < -10:
+        player.y = 30
+
 def input(key):
+    global position
     hit_info = raycast(camera.world_position, camera.forward, distance=100)
 
     if key == 'left mouse down':
         if hit_info.hit:
-            position = hit_info.entity.position + hit_info.normal
-            client_socket.send(position.encode('utf-8'))
 
+            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client_socket.connect((HOST, int(PORT)))
+
+            position = hit_info.entity.position + hit_info.normal
+            client_socket.send(str(tuple(position)).encode('utf-8'))
+            
             position = client_socket.recv(1024).decode('utf-8')
+            position = Vec3(ast.literal_eval(position))
+            
+            print(position)
             Voxel(position=position)
-            client_socket.close()
 
     if key == 'right mouse down' and mouse.hovered_entity:
         destroy(mouse.hovered_entity)
 
 player = FirstPersonController()
 app.run()
+# client_socket.close()
