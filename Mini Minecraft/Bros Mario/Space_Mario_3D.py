9c7d867cf5bed4bab49e6a5f3be03fb31f57d26d
imvickykumar999 <imvickykumar999@gmail.com>
2023-05-03 13:40:16 +0530

    collider

--- Mini Minecraft/Bros Mario/Space_Mario_3D.py
+++ Mini Minecraft/Bros Mario/Space_Mario_3D.py
@@ -1,11 +1,40 @@
 
 import time
 from ursina import *
+from random import randint
 from ursina.prefabs.first_person_controller import FirstPersonController
 
 app = Ursina()
 t = time.time()
+window.fullscreen = True
+
+player = FirstPersonController(collider='box')
+player.gravity = 10e-2
+player.y = 3
+player.z = 3
+player.x = 1
+
+ball = Entity(
+    scale=.5,
+    collider='box',
+    model='sphere',
+    color=color.red,
+    position=(0,1,110),
+    texture='circle_outlined',
+)
+
+box_1 = Entity( 
+    model='cube',
+    scale=(2,4,2),
+    collider='box',
+    color=color.white,
+    position=(8,1,110), 
+    texture='static/wall.png',
+)
+
+box_2 = duplicate(box_1, x=-8)
 cont = True
+dx=.1
 
 mario  = Audio('static/super-mario-bros.mp3',  loop = True,  autoplay = True)
 fall   = Audio('static/Super Mario Death.mp3', loop = False, autoplay = False)
@@ -35,13 +64,10 @@ for y in range(3,-1,-1):
 
 for y in range(4):
     for z in range(y+110, 216, 13):
-        Voxel(
-        position=(1,y,z),
-        color=color.yellow,
-        )
+        Voxel(position=(1,y,z))
 
 def update():
-    global deatils, player, won, cont
+    global deatils, player, won, cont, dx
 
     if (player.z < 212 and player.y < -2) or (time.time() - t > 100):
         won.text = 'You Lost'
@@ -65,6 +91,10 @@ def update():
     Time left = {int(100 - (time.time() - t))} sec.
     '''
 
+    ball.x += dx
+    if ball.intersects().hit:
+        dx =- dx # bounce back
+
 def input(key):
     if key == 'right mouse down' and mouse.hovered_entity:
         destroy(mouse.hovered_entity)
@@ -79,13 +109,6 @@ won = Text(
     origin=(0,-4),
 	color=color.green,
 	) 
-
-window.fullscreen = True
-player = FirstPersonController()
-player.gravity = 10e-2
-player.y = 100
-player.z = 2
-player.x = 1
 
 skybox_image = load_texture("static/space.png")
 sky = Sky(texture=skybox_image)
