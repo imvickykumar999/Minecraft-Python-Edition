d07fc32ca93d2a67b82722dcd7463d1206950049
imvickykumar999 <imvickykumar999@gmail.com>
2023-07-30 22:00:51 +0530

    /gamemode

--- MCPi Server/main.py
+++ MCPi Server/main.py
@@ -2,8 +2,8 @@
 # pip install mcpi
 from mcpi.minecraft import Minecraft 
 
-mc = Minecraft.create(address = 'localhost', port = 52751) 
-mc.postToChat('Hello World')
+mc = Minecraft.create(address = 'localhost', port = 50657) 
+mc.postToChat('/gamemode survival')
 
 pos = mc.player.getPos()
 print(pos)
